b6876701a7fdf9f6a63a860672b32b278c82394e
zhyq0826 <zhyq0826@126.com>
2017-05-19 20:29:08 +0800

    update

--- src/github.com/zhyq0826/learngo/p.go
+++ src/github.com/zhyq0826/learngo/p.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type NameAge struct {
+	name string
+	age  int
+}
+
+func (n *NameAge) PlusAge(x int) {
+	n.age += x
+}
+
+type SecondNameAge NameAge
+
+type ThirdNameAge struct {
+	NameAge
+}
+
 func main() {
 	var p *int //声明一个指针
 	fmt.Printf("%v", p)
@@ -17,4 +32,17 @@ func main() {
 	fmt.Printf("%v", p)
 	println("")
 	fmt.Printf("%v", *p) //输出地址指向的值
+
+	a := new(NameAge)
+	a.age = 10
+	a.name = "hello"
+	fmt.Printf("%v\n", a)
+	a.PlusAge(10)
+	fmt.Printf("%v\n", a)
+
+	b := new(ThirdNameAge)
+	b.age = 10
+	b.name = "third"
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", b.NameAge)
 }
