bcbd91f02a9734e25e3d1cfdb08c73f5ddebfb84
zhyq0826 <zhyq0826@126.com>
2017-10-23 17:29:47 +0800

    update

--- learngo/go_01.go
+++ learngo/go_01.go
@@ -10,22 +10,20 @@ type field struct {
 }
 
 func (p *field) print() {
-	fmt.Printf("func %#v\n", p)
-	fmt.Printf("func %T\n", p)
-	fmt.Printf("func %v\n", p)
-	fmt.Println(p.name)
+	fmt.Printf("func p point to value %v\n", p)
+	fmt.Printf("func p store value %p\n", p)
 }
 
 func main() {
 	data := []field{{"one"}, {"two"}, {"three"}}
 
-	// 在 for 循环中变量 v 是单一变量，go 使用 v.print 执行时必须进行隐士转换，goroutine 的执行极有可能是发生在 for 循环之后，
-	// 此时 v 指向的是最后一个 &v 相当于是 {“three”} 的指针
+	// 在 for 循环中变量 v 是单一变量，go 使用 v.print 执行时必须进行隐士转换，goroutine 的执行极有可能是发生在 for 循环之后
 	for _, v := range data {
 		pp := (*field).print
-		fmt.Printf("for %#v\n", v)
+		fmt.Printf("for v value %v\n", v)    // {one}
+		fmt.Printf("for v address %p\n", &v) // v 的地址，在这里是相同的
+		//go 是按值传递，传递的是 &v 的拷贝，而 &v 其实一直都指的是 v 这个变量，是 v 本身的拷贝，v 在 for 结束后变成了 {three}
 		go pp(&v)
-		// go v.print()
 	}
 
 	time.Sleep(3 * time.Second)
--- learngo/go_02.go
+++ learngo/go_02.go
@@ -10,22 +10,22 @@ type field struct {
 }
 
 func (p *field) print() {
-	fmt.Printf("func %#v\n", p)
-	fmt.Printf("func %T\n", p)
-	fmt.Printf("func %v\n", p)
-	fmt.Println(p.name)
+	fmt.Printf("func p point to value %v\n", p)
+	fmt.Printf("func p store value %p\n", p)
 }
 
 func main() {
 	data := []*field{{"one"}, {"two"}, {"three"}}
 
-	// v 是指针，go 不需要进行转换，直接把这个指针传递进行了，这个传递发生在 go 直接使用时
+	// 在 for 循环中变量 v 是单一变量，go 使用 v.print 执行时必须进行隐士转换，goroutine 的执行极有可能是发生在 for 循环之后
 	for _, v := range data {
 		pp := (*field).print
-		fmt.Printf("for %#v\n", v)
+		fmt.Printf("for v value %v\n", v)    // {one}
+		fmt.Printf("for v address %p\n", &v) // v 的地址，在这里是相同的
+		//go 是按值传递，传递的是 v 的拷贝，而 v 是一个指向 filed 的指针，传递的是指针的拷贝，而这个指针在三次都是不一样的
 		go pp(v)
 	}
 
 	time.Sleep(3 * time.Second)
-	//goroutines print: three, three, three
+	//goroutines print: one, two, three
 }
