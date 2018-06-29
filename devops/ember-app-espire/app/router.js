c15c9a42b164425fa56722afa93f89087b82a362
ssss <zhyq0826@126.com>
2018-06-29 15:52:33 +0800

    update

---
+++ devops/ember-app-espire/app/router.js
@@ -0,0 +1,15 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('domain');
+  this.route('computer');
+  this.route('app');
+});
+
+export default Router;
