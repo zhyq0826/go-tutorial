76d56ffa645c212c0c4bc2021ea5fb564fbee055
ssss <zhyq0826@126.com>
2018-08-14 18:48:53 +0800

    rm devops

--- devops/ember-app-espire/app/instance-initializers/session.js
+++
@@ -1,9 +0,0 @@
-export function initialize(/*appInstance*/) {
-    // let session = appInstance.lookup('service:session');
-    // let store = appInstance.lookup('service:store');
-    // session.loadCurrentUser();
-}
-
-export default {
-  initialize
-};
