56000c7d6ece04179c1149e6107601dfdbe180c1
ssss <zhyq0826@126.com>
2018-09-11 19:38:07 +0800

    update

---
+++ topic/mux/main_04.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	mu      sync.Mutex
+	balance int
+)
+
+func Balance() int {
+	fmt.Println("balance wait for another goroutine release lock")
+	mu.Lock()
+	defer mu.Unlock()
+	return balance
+}
+
+func Balance2() int {
+	mu.Lock()
+	defer mu.Unlock()
+	time.Sleep(10 * time.Second)
+	return balance
+}
+
+func Deposit(amount int) {
+	mu.Lock()
+	deposit(amount)
+	mu.Unlock()
+}
+func Withdraw(amount int) bool {
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(-amount)
+	if balance < 0 {
+		deposit(amount)
+		return false // insufficient funds
+	}
+	return true
+}
+func deposit(amount int) {
+	balance = balance + amount
+}
+
+func main() {
+	balance = 100
+	wait := make(chan int)
+	go func() {
+		fmt.Println("balance 2 == >", Balance2())
+		wait <- 1
+	}()
+	fmt.Println("balance ==>", Balance())
+	<-wait
+}
